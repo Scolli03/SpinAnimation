553788c37a6f1262f585a2ff2e9a444ac3683d14
Shain Collins <Scolli03.sc@gmail.com>
2020-09-25 11:03:38 -0500

    added comments

--- SpinAnimation/MainWindow.xaml.cs
+++ SpinAnimation/MainWindow.xaml.cs
@@ -21,37 +21,66 @@ namespace SpinAnimation
     /// </summary>
     public partial class MainWindow : Window
     {
+        //The MainWindows instance of the VM class
         VM vm;
         public MainWindow()
         {
             InitializeComponent();
+
+            //set MainWindows vm to new instance of the VM class
             vm = new VM();
+
+            //this sets the MainWindow's Context to that of the VM instance
+            //this is why when you bind to the AngleValue in XAML it binds 
+            //to the VM instances property, not a property on the MainWindow
             this.DataContext = vm;
         }
 
+        /// <summary>
+        /// button event handler, generates a new angle and sets
+        /// the VM instances AngleValue property which fires the property changed event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             Random rnd = new Random();
-            vm.AngleValue = rnd.Next(360,3600);
+
+            //this part is wierd, i was trying to set it so it always 
+            //makes at lease one revolution but this is not always true
+            vm.AngleValue = rnd.Next(3600);
         }
 
+        /// <summary>
+        /// This class can be moved into another file and referenced in the MainWindow.cs
+        /// </summary>
         class VM : INotifyPropertyChanged
         {
+            //private backing field
             private int angleValue;
+
+            //public property
             public int AngleValue
             {
                 get
                 {
+                    //returns the private field when calling the public property
                     return this.angleValue;
                 }
                 set
                 {
+                    //sets the private field when setting the public property
+                    //and raises the property changed event
                     this.angleValue = value;
                     RaisePropertyChanged("AngleValue");
                 }
             }
 
+            //event handler for property changed
             public event PropertyChangedEventHandler PropertyChanged;
+
+            //method that uses the PropertyChanged even to determine if the
+            //proptery did in fact change and notifies if it has
             void RaisePropertyChanged(string propertyName)
             {
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
